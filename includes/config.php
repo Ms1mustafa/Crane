e96ee755c0181a6bd878618007fac2376b985600
gj-0l <119807853+gj-0l@users.noreply.github.com>
2024-03-13 22:54:42 +0300

    merge

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+ob_start();
+
+date_default_timezone_set('Asia/Baghdad');
+
+try {
+    $con = new PDO("mysql:dbname=crane;host:localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch (PDOException $e) {
+    exit("Connection failed: " . $e->getMessage());
+}
